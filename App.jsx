b1218cd9c1bf2c5fa7be442c0c6978645b4b5efd
= <robfoggo@gmail.com>
2016-01-24 13:30:28 -0500

    task functionality, add, remove

--- App.jsx
+++ App.jsx
@@ -5,11 +5,28 @@ App = React.createClass({
   //  use data from a Meteor collection inside a React component
   mixins: [ReactMeteorData],
 
-  // { _id: 1, text: "Move Tasks collection to 'both' directory" },
+  // using the createdAt field added by our new code sort the TODO list to place newer items on top
   getMeteorData() {
     return {
-      tasks: Tasks.find({}).fetch()
+      tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+      //tasks: Tasks.find({}).fetch()
     }
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+ 
+    // Find the text field via the React ref
+    var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    // we are adding a task to the tasks collection by calling Tasks.insert().
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+ 
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = "";
   },
 
   // Loads items from the Tasks collection and puts them on this.data.tasks
@@ -24,6 +41,14 @@ App = React.createClass({
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          {/* onSubmit attribute that references a method on the component called handleSubmit. In React, this is how you listen to browser events, like the submit event on the form. The input element has a ref property which will let us easily access this element later.
+           */}
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
         </header>
  
         <ul>
--- Task.jsx
+++ Task.jsx
@@ -5,9 +5,43 @@ Task = React.createClass({
     // We can use propTypes to indicate it is required
     task: React.PropTypes.object.isRequired
   },
+
+  /*
+    The update function on a collection takes two arguments. The first is a selector that identifies a subset of the collection, and the second is an update parameter that specifies what should be done to the matched objects.In this case, the selector is just the _id of the relevant task. The update parameter uses $set to toggle the checked field, which will represent whether the task has been completed.
+  */
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this.props.task._id, {
+      $set: {checked: ! this.props.task.checked}
+    });
+  },
+
+  /*
+    The code below uses Tasks.remove to delete a task. The remove function takes one argument, a selector that determines which item to remove from the collection.
+  */
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  },
+
   render() {
+    // Give tasks a different className when they are checked off,
+    // so that we can style them nicely in CSS
+    const taskClassName = this.props.task.checked ? "checked" : "";
+    
     return (
-      <li>{this.props.task.text}</li>
+      <li>{taskClassName}
+        <button className="delete" onClick={this.deleteThisTask}>
+          &times;
+        </button>
+ 
+        <input
+          type="checkbox"
+          readOnly={true}
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked} />
+ 
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 });
